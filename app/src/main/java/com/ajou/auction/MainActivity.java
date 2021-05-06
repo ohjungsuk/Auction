19f3d5470063590d15ee71e5a3896108408723b3
SH0610 <sanghee0610@ajou.ac.kr>
2021-05-06 13:17:15 +0900

    하단 메뉴와 프래그먼트 추가

--- app/src/main/java/com/ajou/auction/MainActivity.java
+++ app/src/main/java/com/ajou/auction/MainActivity.java
@@ -1,14 +1,64 @@
 package com.ajou.auction;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    // FrameLayout에 각 메뉴의 Fragment를 바꿔 줌
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    // 4개의 메뉴에 들어갈 Fragment들
+    private Menu1Fragment menu1Fragment = new Menu1Fragment();
+    private Menu2Fragment menu2Fragment = new Menu2Fragment();
+    private Menu3Fragment menu3Fragment = new Menu3Fragment();
+    private Menu4Fragment menu4Fragment = new Menu4Fragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // 첫 화면 지정
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_layout, menu1Fragment).commitAllowingStateLoss();
+
+        // bottomNavigationView의 아이템이 선택될 때 호출될 리스너 등록
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (item.getItemId()) {
+                    case R.id.menu1: {
+                        transaction.replace(R.id.frame_layout, menu1Fragment).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.menu2: {
+                        transaction.replace(R.id.frame_layout, menu2Fragment).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.menu3: {
+                        transaction.replace(R.id.frame_layout, menu3Fragment).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.menu4: {
+                        transaction.replace(R.id.frame_layout, menu4Fragment).commitAllowingStateLoss();
+                        break;
+                    }
+                }
+
+                return true;
+            }
+        });
     }
 }
